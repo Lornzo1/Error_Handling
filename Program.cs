042ba04f3a8497dcb469a4bcc9ac7fb05c33ef92
Lorenzo Usai <UsaiL@StPaulsSchool.org.uk>
2020-10-14 21:25:45 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace Error_Handling
+{
+    class Program
+    {
+        static double Sqrt()
+        {
+            Console.WriteLine("input the number");
+            var t = Console.ReadLine();
+            double a;
+            try
+            {
+                a = Math.Sqrt(Convert.ToDouble(t));
+            }
+            catch (SystemException)
+            {
+                a =  Sqrt();
+            }
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Sqrt());
+        }
+    }
+}
